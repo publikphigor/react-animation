9a60f58d872206907ff3db872c1c9cc762a775f0
Koladele Olaitan <koladeleolaitan@gmail.com>
2022-09-17 00:59:47 +0000

    Created starter files.

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import { useState } from "react";
+
+function App() {
+  return (
+    <div className="App">
+      <p className="read-the-docs">Click on the Vite and React logos to learn more</p>
+    </div>
+  );
+}
+
+export default App;
